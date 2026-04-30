9ed52ed285c2edea371c4d3c616756e3190761f3
Sarvar55 <sarvar.musazade@gmail.com>
2026-04-30 18:27:28 +0400

    feat(docs): add JWT Bearer authentication to Swagger UI
    Configure OpenAPI SecurityScheme so Swagger UI shows an 'Authorize'
    button where developers can paste their JWT token to test protected
    endpoints without needing external tools like Postman.

--- src/main/java/org/karar/dev/config/OpenApiConfig.java
+++ src/main/java/org/karar/dev/config/OpenApiConfig.java
@@ -1,27 +1,78 @@
 package org.karar.dev.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String API_TITLE = "Karar Dev API";
+    private static final String API_VERSION = "1.0";
+    private static final String API_DESCRIPTION = "API documentation for Karar Dev Application";
+
+    private static final String CONTACT_NAME = "Karar Dev Team";
+    private static final String CONTACT_EMAIL = "contact@karar.dev";
+
+    private static final String LICENSE_NAME = "Apache 2.0";
+    private static final String LICENSE_URL = "https://springdoc.org";
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+    private static final String SECURITY_SCHEME = "bearer";
+    private static final String BEARER_FORMAT = "JWT";
+    private static final String SECURITY_DESCRIPTION = "Enter JWT token. Example: eyJhbGciOiJIUzI1NiJ9...";
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Karar Dev API")
-                        .version("1.0")
-                        .description("API documentation for Karar Dev Application")
-                        .contact(new Contact()
-                                .name("Karar Dev Team")
-                                .email("contact@karar.dev"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://springdoc.org")));
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement())
+                .components(apiComponents());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title(API_TITLE)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .contact(apiContact())
+                .license(apiLicense());
+    }
+
+    private Contact apiContact() {
+        return new Contact()
+                .name(CONTACT_NAME)
+                .email(CONTACT_EMAIL);
+    }
+
+    private License apiLicense() {
+        return new License()
+                .name(LICENSE_NAME)
+                .url(LICENSE_URL);
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement()
+                .addList(SECURITY_SCHEME_NAME);
+    }
+
+    private Components apiComponents() {
+        return new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, jwtBearerSecurityScheme());
+    }
+
+    private SecurityScheme jwtBearerSecurityScheme() {
+        return new SecurityScheme()
+                .name(SECURITY_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(SECURITY_SCHEME)
+                .bearerFormat(BEARER_FORMAT)
+                .description(SECURITY_DESCRIPTION);
     }
 }
